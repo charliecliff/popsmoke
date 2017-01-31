d52bc053b6ef6bbaf5888ab9337d1613f109d66d
ccliff <charlie.cliff@gmail.com>
2017-01-31 10:06:16 -0600

    Adding the Facebook SDK

---
+++ popsmoke/ViewControllers/PSLoginViewController.swift
@@ -0,0 +1,47 @@
+//
+//  PSLoginViewController.swift
+//  popsmoke
+//
+//  Created by Charles Cliff on 1/31/17.
+//  Copyright © 2017 Charles Cliff. All rights reserved.
+//
+
+import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+
+class PSLoginViewController: UIViewController {
+
+	@IBOutlet weak var facebookLoginButton: FBSDKLoginButton?
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+		self.facebookLoginButton?.readPermissions = ["public_profile", "email", "user_friends"]
+		self.facebookLoginButton?.delegate = self
+    }
+}
+
+extension PSLoginViewController: FBSDKLoginButtonDelegate {
+	
+	func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+		
+		if ((error) != nil) {
+			
+			return
+		}
+		if result.isCancelled {
+			
+			return
+		}
+		else {
+			let token = FBSDKAccessToken.current().tokenString
+			
+		}
+	}
+	
+	func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+		
+		print("User Logged Out")
+	}
+}
